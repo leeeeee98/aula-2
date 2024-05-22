f65df4a3cab38a34c351f23583752ba16af814bd
SARAH SANTOS DE OLIVEIRA <00001093490068sp@aluno.educacao.sp.gov.br>
2024-05-22 17:53:03 -0300

    primeiro commit

---
+++ exercicio3.py
@@ -0,0 +1,5 @@
+quantidade_desejada= int(input("digite a quantidade desejada: "))
+if quantidade_desejada >=10:
+    print("produto disponivel em estoque. Aproveite!")
+else:
+    print("produto indisponivel no momento. Volte mais tarde!")
---
+++ exercício 4.py
@@ -0,0 +1,5 @@
+nota_final = float(input("digite a nota final: "))
+if nota_final >= 7:
+    print("parabens voce foi aprovado.")
+else:
+    print("infelizmente, voce nao foi aprovado. estude mais. ")
